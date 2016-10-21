065bf6df37c562bd8c08e0998f80b2c6a2ef56bc
usuario <usuario@10.22.143.60>
2016-10-21 09:36:24 +0100

    Kata3 V2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -10,9 +10,11 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame{
-
-    public HistogramDisplay() {
+    private final Histogram<String> histogram;
+    
+    public HistogramDisplay(Histogram<String> histogram) {
         super("HISTOGRAMA");
+        this.histogram = histogram;
         setContentPane(createPanel());
         pack();
     }
@@ -28,7 +30,7 @@ public class HistogramDisplay extends ApplicationFrame{
     }
     
     private JFreeChart createChart(DefaultCategoryDataset dataSet) {
-        JFreeChart chart = ChartFactory.createBarChart( 
+        JFreeChart chart = ChartFactory.createBarChart3D( 
                                 "JFreeChart Histogram", 
                                 "Dominios email", 
                                 "Nº de emails", 
@@ -43,10 +45,9 @@ public class HistogramDisplay extends ApplicationFrame{
     
     private DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(5,"","gmail.com");
-        dataSet.addValue(10,"","ulpgc.es");
-        dataSet.addValue(8,"","ull.es");
-        dataSet.addValue(2,"","hotmail.com");
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), key, key);
+        }
         return dataSet;
     }
 }
